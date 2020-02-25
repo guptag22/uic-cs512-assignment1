b51a99e39c0f16659600cc3c2ef3d69ff569f9c6
Garima <73garima@gmail.com>
2020-02-25 00:01:36 -0600

    Update 2_garima.py

--- code/2_garima.py
+++ code/2_garima.py
@@ -5,13 +5,15 @@ import scipy.optimize as opt
 import string
 from numpy.linalg import norm
 
-train_data_PATH = os.path.join(os.path.dirname(os.getcwd()), "data", "train.txt")
-train_data = np.genfromtxt(train_data_PATH, delimiter = ' ')
+train_data_PATH = "/Users/gammu/Documents/AML_assignment_1/uic-cs512-assignment1/data/train.txt"
+file = open(train_data_PATH,"r")
+train_data = (file.read()).strip()
 
-test_data_PATH = os.path.join(os.path.dirname(os.getcwd()), "data", "test.txt")
-test_data = np.genfromtxt(test_data_PATH, delimiter = ' ')
+test_data_PATH = "/Users/gammu/Documents/AML_assignment_1/uic-cs512-assignment1/data/test.txt"
+file = open(test_data_PATH,"r")
+test_data = (file.read()).strip()
 
-model_PATH = os.path.join(os.path.dirname(os.getcwd()), "data", "model.txt")
+model_PATH = "/Users/gammu/Documents/AML_assignment_1/uic-cs512-assignment1/data/model.txt"
 model = np.genfromtxt(model_PATH, delimiter = ' ')
 
 def get_lhood_grad(Yt,Xt,W,T) :         ## get log(p(Yt|Xt)) and gradients for one training example
@@ -85,15 +87,28 @@ def crf_obj(model,word_list,C) :
     W = np.reshape(W, (26,128))                 ## W contains each Wy as a row of size 128
     T = np.reshape(T, (26,26))
     T = T.transpose()
-    
+    word_list = word_list.split('\n')
+    N = int((word_list[-1].split())[3])         ## Total number of words in the training data
     ##### Extract Y and X from data ######
     ##### shape of X should be each letter in a row of size 128
-    ##----------- TO-DO -----------##
-    # Calculate gradient of whole training data
-    # Define N = Total number of words in the training data
-    ##----------- TO-DO -----------##
-    grad_Wy = np.empty((26,128))
-    grad_T = np.empty((26,26))
+    Y = []              ## array of all labels of all words
+    X = []              ## array of pixel values of all words
+    y = []              ## array of labels of letters for a single word
+    x = []              ## array of pixel values of letters for a single word
+    for i in range(len(train_data)) :
+        letter = (train_data[i]).split()        ## extract data corresponding to a single letter
+        y.append(ord(letter[1].lower())-97)     ## extract the label and convert it to integer
+        x.append(np.array(list(map(int,letter[5:]))))       ## extract pixel values, convert them to integer 
+                                                            ## and form a numpy array for data manipulation
+        if (int(letter[2]) == -1) :             ## Check for end of word
+            Y.append(y)
+            X.append(np.array(x))
+            y = []
+            x = []
+    
+    ## Calculate gradient of whole training data
+    grad_Wy = np.zeros((26,128))
+    grad_T = np.zeros((26,26))
     log_liklihood = 0
     for t in range(N) :
         log_p_Yt, grad_Wy_t, grad_Tij_t = get_lhood_grad(Y[t], X[t], W, T)
@@ -102,8 +117,12 @@ def crf_obj(model,word_list,C) :
         grad_T += grad_Tij_t / N
     obj = ((norm(W)**2 + norm(T)**2) /2) - C * log_liklihood
     ## flatten grad_Wy and grad_T and concatenate
-    ##----------- TO-DO -----------##
-    grad_theta = flat_grad_Wy + flat_grad_T
+    flat_grad_Wy = []
+    flat_grad_T = []
+    for i in range(26) :
+        flat_grad_Wy.append(grad_Wy[i,:])
+        flat_grad_T.append(grad_T[:,i])
+    grad_theta = flat_grad_Wy + flat_grad_T         ## Gradient vector
     return [obj, grad_theta]
 
 def crf_test(model, word_list) :
@@ -112,9 +131,24 @@ def crf_test(model, word_list) :
     W = np.reshape(W, (26,128))                 ## W contains each Wy as a row of size 128
     T = np.reshape(T, (26,26))
     T = T.transpose()
+    word_list = word_list.split('\n')
+    # N = int((word_list[-1].split())[3]) 
     ##### Extract Y and X from data ######
     ##### shape of X should be each letter in a row of size 128
-    ##----------- TO-DO -----------##
+    Y = []              ## array of all labels of all words
+    X = []              ## array of pixel values of all words
+    y = []              ## array of labels of letters for a single word
+    x = []              ## array of pixel values of letters for a single word
+    for i in range(len(train_data)) :
+        letter = (train_data[i]).split()        ## extract data corresponding to a single letter
+        y.append(ord(letter[1].lower())-97)     ## extract the label and convert it to integer
+        x.append(np.array(list(map(int,letter[5:]))))       ## extract pixel values, convert them to integer 
+                                                            ## and form a numpy array for data manipulation
+        if (int(letter[2]) == -1) :             ## Check for end of word
+            Y.append(y)
+            X.append(np.array(x))
+            y = []
+            x = []
     y_predict = crf_decode(W, T, word_list)     ## Decode utility defined in 1c
 
     ## Calculate accuracy by comparing y_pred with true labels
